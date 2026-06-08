5ce7e34e4f5bf894ad3de2675e9115b9a6ef8908
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-08 13:59:28 +0530

    messageRotes

---
+++ backend/src/routes/messageRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const {
+  getMessages,
+} = require("../controllers/messageController");
+
+const router = express.Router();
+
+router.get("/", getMessages);
+
+module.exports = router;
