6438ad8dc135490f9801e4937382bd9995d000d5
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-08 13:46:44 +0530

    roomRoutes

---
+++ backend/src/routes/roomRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const {
+  createRoom,
+  getRooms,
+} = require("../controllers/roomController");
+
+const router = express.Router();
+
+router.post("/", createRoom);
+
+router.get("/", getRooms);
+
+module.exports = router;
