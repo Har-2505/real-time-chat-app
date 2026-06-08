3e853b496379ef8138e7faf41ff75a03d5408f28
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-08 16:52:25 +0530

    Added Time stamp

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -2,20 +2,13 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   {
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    room: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Room",
-    },
-    content: {
-      type: String,
-      required: true,
-    },
+    username: String,
+    room: String,
+    content: String,
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Message", messageSchema);
