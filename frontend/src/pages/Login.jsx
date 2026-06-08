c78c684512c58fe13993fd4a93b08da868cb0262
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-08 14:59:51 +0530

    login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import API from "../services/api";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem(
+        "token",
+        res.data.token
+      );
+
+      localStorage.setItem(
+        "user",
+        JSON.stringify(res.data.user)
+      );
+
+      alert("Login Successful");
+    } catch (error) {
+      alert(error.response?.data?.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) =>
+            setEmail(e.target.value)
+          }
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) =>
+            setPassword(e.target.value)
+          }
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
