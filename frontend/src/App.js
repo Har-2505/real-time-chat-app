7ca71ff837ec04278a08cf3b164d4b49ff3cc7a8
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-08 15:12:57 +0530

    app changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Rooms from "./pages/Rooms";
+import ChatRoom from "./pages/ChatRoom";
 
 function App() {
   return (
@@ -9,9 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/chat/:roomName" element={<ChatRoom />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
+//123
 export default App;
